9fc77652e584b5102972af71245f521405900587
TheBharatMishra <mishrabm2004@gmail.com>
2023-08-16 11:20:45 -0700

    Basic Javascript

---
+++ js-basics/introduction.js
@@ -0,0 +1,24 @@
+// This is a comment
+
+/*
+This is
+a multiline
+comment
+*/
+
+//Declaring a variable
+var myName;
+
+//Storing values in the variables using assigment operator "=".
+var myVal = 69;
+
+var realVal;
+realVal = 420;
+
+//Assigning the values of one variable to another variable.
+var myVal = realVal;
+
+//Assigning the string values to the variable.
+var myLang = "Javascript";
+
+// Concatinating variables
