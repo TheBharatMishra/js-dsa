c01cf4a5a6c41b8932f18406e4bfee612656c3ec
TheBharatMishra <mishrabm2004@gmail.com>
2023-08-17 23:02:15 +0530

    JS Basics

--- js-basics/introduction.js
+++ js-basics/introduction.js
@@ -28,3 +28,17 @@ console.log(conVar);
 
 //camelCase is a good practice while declaring variables.
 var camelCase = "This is a camel case variable.";
+
+// var vs let (let is preffered)
+var megaVar = 56;
+var megaVar = 65; // Can declare the variable next time using var.
+
+let segVar = 45;
+// let segVar = 34; Cannot declare the variable next time while using let
+
+//Using const keyword while declaring variables.
+const SOMO = 69; // Using UPPERCASE for the immutable types is considered good.
+//const is a Read-only type.
+
+//Adding Numbers in JS.
+const SUM = 10 + 10;
